40ab45051579990213875b60ce27b46f598c4321
yu.zuomin <yu.zuomin@ebiz-digits.com>
2021-08-03 15:28:52 +0800

    init step4

---
+++ simple-step4/src/main/java/com/baogex/springframework/beans/factory/BeanFactory.java
@@ -0,0 +1,30 @@
+package com.baogex.springframework.beans.factory;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author : baogex.com
+ * @date : 2021-08-02
+ */
+public interface BeanFactory {
+
+    /**
+     * 获取一个bean实例对象
+     *
+     * @param beanName bean名称（id)
+     * @return 返回一个bean实例对象
+     */
+    Object getBean(String beanName);
+
+
+    /**
+     * 根据入参列表指定构造函数获取bean
+     *
+     * @param beanName 名称
+     * @param args     参数列表
+     * @return 返回一个创建好的bean实例对象，如果不存在则返回null
+     */
+    Object getBean(String beanName, Object... args);
+}
---
+++ simple-step4/src/test/java/com/baogex/springframework/beans/factory/service/SimpleDao.java
@@ -0,0 +1,23 @@
+package com.baogex.springframework.beans.factory.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author : baogex.com
+ * @since : 2021-08-03
+ */
+public class SimpleDao {
+    private Map<String, String> userData = new HashMap<>(4);
+
+    {
+        userData.put("1", "小明");
+        userData.put("2", "中明");
+        userData.put("3", "大明");
+        userData.put("4", "究明");
+    }
+    
+    public String getUserNameById(String id){
+        return userData.get(id);
+    }
+}
